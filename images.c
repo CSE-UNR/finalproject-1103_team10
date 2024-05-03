2264533238888fbdbf02593458b9089c9d185c8e
1103-Wilfong-Jessi <jwilfong@nevada.unr.edu>
2024-05-03 17:21:26 +0000

    final

---
+++ images.c
@@ -0,0 +1,31 @@
+//
+
+#include <stdio.h>
+
+int main(){
+
+//menu 1
+	printf("**ERINSTAGRAM** \n");
+	printf("1: Load image \n");
+	printf("2: Display image \n");
+	printf("3: Edit image \n");
+	printf("0: Exit \n");
+	printf("\n");
+	printf("Choose from one of the options above: ");
+
+	
+//menu 2	
+
+	printf("**EDITING** \n");
+	printf("1: Crop image \n");
+	printf("2: Dim image \n");
+	printf("3: Brighten image \n");
+	printf("4: Rotate image \n");
+	printf("0: Return to main menu \n");
+	printf("\n");
+	printf("Choose from one of the options above: ");
+
+
+return 0;
+}
+
