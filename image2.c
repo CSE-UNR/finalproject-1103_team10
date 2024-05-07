43fef425507f1cb679ed2a58bbf180a27e0779d3
Evan Sean Closas <closasevan@nevada.unr.edu>
2024-05-07 21:56:24 +0000

    image2.c

--- image2.c
+++ image2.c
@@ -3,25 +3,30 @@
 //Final Project
 
 #include <stdio.h>
-
 #define SIZE 100
 #define ROW 12
 #define COL 21
 
-int load(char name[], char array[][COL]);
+int load(FILE *fileIn, char array[][COL]);
 void display(char name[]);
 void editMenu();
-void crop();
-void dimBright(int brightness, char array [][COL];
+void crop(int *cRowPtr, int *cColPtr, char array[][COL], int arrayC[][COL]);
+void bright(char array [][COL]);
+void dim(char array [][COL]);
 void rotate();
 int save(char array[][COL]);
 
 int main(){
 
-int input;
-char name[SIZE];
-char array[ROW][COL];
-
+	int input;
+	char name[SIZE];
+	char array[ROW][COL];
+	int arrayC[ROW][COL];
+	FILE *fileIn; 
+	int cropRptr, cropCptr;
+	
+//	cropRptr = crop(&cRowPtr, &cColPtr, array, arrayc);
+	
 //menu 1
 	do{
 	printf("\n");
@@ -37,7 +42,7 @@ char array[ROW][COL];
 	
 	switch(input){
 	case 1:
-		load(name, array);
+		load(fileIn, name, array);
 	break;
 	case 2:
 		display(name);
@@ -61,33 +66,41 @@ char array[ROW][COL];
 return 0;
 }
 
-int load(char name[], char array[][COL]){
-	
+int load(FILE *fileIn, char array[][COL]){
+	char digit;
+	int rowNum = 0, colNum = 0;
  
 	printf("\n");
 	printf("What is the name of the image file? ");
 	scanf("%s", name);
 	
-	FILE *fileIn = fopen(name, "r");
+	fileIn = fopen(name, "r");
 	
 	
 	if(fileIn == NULL){
-	printf("\n");
-	printf("Could not find an image with that filename. \n");
-	return 0;
-	}else{
-	printf("\n");
-	printf("Image successfully loaded! \n");
+		printf("\n");
+		printf("Could not find an image with that filename. \n");
+		return 0;
 	}
-	for(int i = 0; i < ROW; i++){
-		if(fgets(array[i], COL + 1, fileIn) == NULL){
-			printf("Could not find an image with that filename. \n");
-			fclose(fileIn);
-			return 0;	
+	
+	while(fscanf(fileIn, "%c", &digit) == 1) {
+			if(digit >= '0' || digit <= '4'){
+				array[rowNum][colNum] == digit;
 			}
-			}
-		fclose(fileIn);
-		return 1;
+	}
+	
+	if (digit == '\n'){
+		if(colNum == 0){
+			colNum++;
+		}
+		rowNum++;
+	}
+	
+	
+	fclose(fileIn);
+	
+	printf("Image Sucessfully Loaded\n");
+	
 }
 
 
@@ -122,7 +135,12 @@ void display(char name[]){
 void editMenu (){
 int input;
 char array[ROW][COL];
-
+cropRptr = crop(&cRowPtr, &cColPtr, array, arrayc);
+int input;
+	char name[SIZE];
+	int arrayC[ROW][COL];
+	FILE *fileIn; 
+	int cropRptr, cropCptr;
 	do{
 	printf("\n");
 	printf("**EDITING** \n");
@@ -137,13 +155,13 @@ char array[ROW][COL];
 	
 	switch(input){
 	case 1: 
-	crop();
+	crop(&cRowPtr, &cColPtr, array, arrayc);
 	break;
 	case 2: 
-	dimBright();
+	bright(array);
 	break;
 	case 3: 
-	dimBright(array);
+	dim(array);
 	break;
 	case 4: 
 	rotate();
@@ -160,29 +178,98 @@ char array[ROW][COL];
 	
 	}
 	
-void crop(){
-	printf("crop \n");
+void crop(int *cRowPtr, int *cColPtr, char array[][COL], int arrayC[][COL]){
+	
+	display(char name[]);
+	
+	int row1, row2, col1, col2;
+	
+	printf("The image you want to crop is 12 x 21");
+	printf("The row starts at 1 & ends at 21, the column value starts at 1 & ends at 12");
+	
+	printf("Which column do you want to be the new left?");
+	scanf("%d", &col1);
+	
+	printf("Which column do you want to be the new right?");
+	scanf("%d", &col2);
+
+	printf("Which row do you want to be the new top?");
+	scanf("%d", &row1);
+
+	printf("Which row do you want to be the new bottom?");
+	scanf("%d", &row2);
+
+	row1--; row2--; col1--; col2--;
+
+	 int cRow = row2 - row1 + 1;
+
+   	 int cCol = col2 - col1 + 1;
+
+
+    if (row1 < 0 || row1 >= ROW || col1 < 0 || col1 >= COL || row2 < 0 || row2 >= ROW || col2 < 0 || col2 >= COL || row2 < row1 || col2 < col1) {
+	printf("Invalid coordinates. Please enter valid coordinates within the image dimensions.\n");
+	return;
 }
-void dimBright(char array [][COL]){
+	for (int i = 0; i < cRow; i++) {
+        	for (int j = 0; j < cCol; j++) {
+			array[ROW][COL] = arrayC[row1 + ROW][col1 + COL];
+
+        	}
+
+    	}
+
+	
+	
+}
+void bright(char array [][COL]){
 	for (int rows = 0; rows < ROW; rows++){
 		for (int column = 0; column < COL; column++){
-			if(array[rows][column] >= '0' && array[rows][column] <= '4'){
-				array[rows][column] += brightness;
+			if(array[rows][column] < 4){
+				array[rows][column]++;
 			}
 		}		
 	}
 }
 
+void dim(char array [][COL]){
+	for (int rows = 0; rows < ROW; rows++){
+		for (int column = 0; column < COL; column++){
+			if(array[rows][column] > 0){
+				array[rows][column]--;
+			}
+		}
+	}
+}
 
 
 void rotate(){
-	printf("rotate \n");
+	
 }
 int save(char array[][COL]){
 	char input;
 	char name[SIZE];
 	
 	//vv needs fixed
+	printf("\n");
+	printf("\n");
+	
+	printf("\n");
+	
+	
+	
+	
+	
+	printf("\n");
+	printf("\n");
+	
+	
+	printf("\n");
+	
+	printf("\n");
+	
+	printf("\n");
+	
+	
 	display(name);
 	//^^
 	printf("\n");
